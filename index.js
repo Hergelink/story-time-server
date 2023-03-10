60210ee498367895c20fb08a2b7a1f1d44e3c337
Hergelink <enver.hergelink@gmail.com>
2023-03-10 10:22:08 +0300

    reverted declaration change

--- index.js
+++ index.js
@@ -105,8 +105,8 @@ app.post('/login', async (req, res) => {
 });
 
 app.get('/profile', (req, res) => {
-  // const { token } = req.cookies;
-  const token = req.cookies.token;
+  const { token } = req.cookies;
+  // const token = req.cookies.token;
 
   jwt.verify(token, secret, {}, (err, info) => {
     if (err) {
